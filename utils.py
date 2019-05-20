aa7081d0a99e217ebecb972e095138c6b4420702
Thomas Cohn <cohnt@umich.edu>
2019-05-20 15:56:20 -0400

    Giving credit for the stackoverflow code.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
 
 def sparseMatrixToDict(mat):
+	# https://stackoverflow.com/questions/52322847/what-is-an-efficient-way-to-convert-an-adjacency-matrix-to-a-dictionary
 	return {i: [j for j, adjacent in enumerate(row) if adjacent] for i, row in enumerate(mat.toarray())}
