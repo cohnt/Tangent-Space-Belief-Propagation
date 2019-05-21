26ebe7fe486f22756687874e05db3a4c4dd7eeab
Thomas Cohn <cohnt@umich.edu>
2019-05-21 12:23:34 -0400

    Generate data, run nearest neighbors, make the graph symmetric, and convert it to a dictionary.

--- bp_isomap.py
+++ bp_isomap.py
@@ -1,25 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+num_iters = 5      # Number of iterations of the message passing algorithm to run
+neighbors_k = 8   # The value of 'k' used for k-nearest-neighbors
+num_points = 500   # Number of data points
+data_noise = 0.001 # How much noise is added to the data
+
 ################
 # Load Dataset #
 ################
 from datasets.dim_2.s_curve import make_s_curve
 
-points, color = make_s_curve(500, 0.001)
+points, color = make_s_curve(num_points, data_noise)
 
 #######################
 # k-Nearest-Neighbors #
 #######################
 from sklearn.neighbors import kneighbors_graph
+from visualization.plot_neighbors import plot_neighbors_2d
 
-neighbor_graph = kneighbors_graph(points, 12, mode="distance", n_jobs=-1)
+neighbor_graph = kneighbors_graph(points, neighbors_k, mode="distance", n_jobs=-1)
+# NOTE: this is not a symmetric matrix.
+
+fig, ax = plt.subplots()
+plot_neighbors_2d(points, color, neighbor_graph, ax)
+plt.show()
 
 ####################
 # Initialize Graph #
 ####################
-from utils import sparseMatrixToDict
+from utils import sparseMatrixToDict, sparseMaximum
 
+# Make the matrix symmetric, and convert it to a dictionary
+neighbor_graph = sparseMaximum(neighbor_graph, neighbor_graph.T)
 neighbor_dict = sparseMatrixToDict(neighbor_graph)
 
 #######################
@@ -32,4 +45,6 @@ neighbor_dict = sparseMatrixToDict(neighbor_graph)
 # Message Passing #
 ###################
 
-# TODO
+for iter_num in range(num_iters):
+	pass
+	# TODO
