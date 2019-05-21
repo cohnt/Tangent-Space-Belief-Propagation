0eb4d09287a6c98025d054710a74cefd4761b3ac
Thomas Cohn <cohnt@umich.edu>
2019-05-21 12:11:44 -0400

    Function to take the elementwise maximum of two sparse matrices.

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ import numpy as np
 def sparseMatrixToDict(mat):
 	# https://stackoverflow.com/questions/52322847/what-is-an-efficient-way-to-convert-an-adjacency-matrix-to-a-dictionary
 	return {i: [j for j, adjacent in enumerate(row) if adjacent] for i, row in enumerate(mat.toarray())}
+
+def sparseMaximum(A, B):
+	# https://stackoverflow.com/questions/19311353/element-wise-maximum-of-two-sparse-matrices
+	BisBigger = A-B
+	BisBigger.data = np.where(BisBigger.data < 0, 1, 0)
+	return A - A.multiply(BisBigger) + B.multiply(BisBigger)
