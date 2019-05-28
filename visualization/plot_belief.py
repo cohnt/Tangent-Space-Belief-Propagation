b4174aaeba75ed197d5afa5a81812a5e939d73c2
Thomas Cohn <cohnt@umich.edu>
2019-05-28 10:35:45 -0400

    Add parameter to change the colormap (if desired).

--- visualization/plot_belief.py
+++ visualization/plot_belief.py
@@ -4,13 +4,13 @@ from matplotlib.collections import LineCollection
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 from utils import Belief
 
-def plot_belief_1d(belief, ax, line_width=1):
+def plot_belief_1d(belief, ax, line_width=1, colormap=plt.cm.seismic):
 	print belief.weights
 	print max(belief.weights)
 	print np.argmax(belief.weights)
 	print belief.pos[np.argmax(belief.weights)]
 	num_points = len(belief.pos)
-	rgb = plt.cm.seismic(belief.weights / max(belief.weights))
+	rgb = colormap(belief.weights / max(belief.weights))
 	line_bottom = 0
 	line_top = 1
 	ax.vlines(belief.pos.flatten(), line_bottom, line_top, colors=rgb, linestyles='solid')
