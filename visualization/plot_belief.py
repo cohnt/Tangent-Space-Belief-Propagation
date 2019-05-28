f858aeed3e8956a6dedf316182bf356aaefc0ea1
Thomas Cohn <cohnt@umich.edu>
2019-05-28 10:37:16 -0400

    Remove print statements, switch to coolwarm colormap.

--- visualization/plot_belief.py
+++ visualization/plot_belief.py
@@ -4,11 +4,7 @@ from matplotlib.collections import LineCollection
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 from utils import Belief
 
-def plot_belief_1d(belief, ax, line_width=1, colormap=plt.cm.seismic):
-	print belief.weights
-	print max(belief.weights)
-	print np.argmax(belief.weights)
-	print belief.pos[np.argmax(belief.weights)]
+def plot_belief_1d(belief, ax, line_width=1, colormap=plt.cm.coolwarm):
 	num_points = len(belief.pos)
 	rgb = colormap(belief.weights / max(belief.weights))
 	line_bottom = 0
