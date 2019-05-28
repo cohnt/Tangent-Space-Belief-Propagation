567e1a3a37265df82db2c28dc32ef76b6838a775
Thomas Cohn <cohnt@umich.edu>
2019-05-28 11:02:32 -0400

    Add function to plot mle estimates in 1d.

--- visualization/plot_belief.py
+++ visualization/plot_belief.py
@@ -9,7 +9,7 @@ def plot_belief_1d(belief, ax, line_width=1, colormap=plt.cm.coolwarm, show_mle=
 	rgb = colormap(belief.weights / max(belief.weights))
 	line_bottom = 0
 	line_top = 1
-	ax.vlines(belief.pos.flatten(), line_bottom, line_top, colors=rgb, linestyles='solid')
+	ax.vlines(belief.pos.flatten(), line_bottom, line_top, colors=rgb, linewidth=line_width, linestyles='solid')
 
 	if show_mle:
 		ind = np.argmax(belief.weights)
@@ -23,3 +23,15 @@ def plot_belief_1d(belief, ax, line_width=1, colormap=plt.cm.coolwarm, show_mle=
 		label_text = "Mean=%f" % pos
 		ax.axvline(pos, line_bottom, line_top, color="black", linewidth=mean_line_width, linestyle="--", dash_capstyle="round")
 		ax.text(pos, 0.05, label_text)
+
+def plot_mle_1d(beliefs, colors, ax, colormap=plt.cm.spectral, line_width=1):
+	num_points = len(beliefs)
+	points = np.zeros(num_points)
+	for i in range(num_points):
+		ind = np.argmax(beliefs[i].weights)
+		points[i] = beliefs[i].pos.flatten()[ind]
+	line_bottom = 0
+	line_top = 1
+	rgb = colormap(colors)
+	print points
+	ax.vlines(points, line_bottom, line_top, colors=rgb, linewidth=line_width, linestyles='solid')
