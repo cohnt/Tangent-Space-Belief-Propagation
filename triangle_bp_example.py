7dcb8eef8e3ef74e0526401a7cc342529f960a3a
Thomas Cohn <cohnt@umich.edu>
2019-05-29 15:30:41 -0400

    Gonna try to get BP working on something simple where I know it will work first.

---
+++ triangle_bp_example.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import sys
+import copy
+from utils import write, flush
+
+num_iters = 20     # Number of iterations of the message passing algorithm to run
