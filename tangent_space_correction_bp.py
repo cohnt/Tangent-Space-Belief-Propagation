aa5990431a96d8dbff251738b9f0c90dfe11bff5
Thomas Cohn <cohnt@umich.edu>
2019-06-10 14:18:19 -0400

    Normally sample ts noisifying angles.

--- tangent_space_correction_bp.py
+++ tangent_space_correction_bp.py
@@ -9,7 +9,7 @@ num_iters = 100     # Number of iterations of the message passing algorithm to r
 neighbors_k = 4    # The value of 'k' used for k-nearest-neighbors
 num_points = 25    # Number of data points
 data_noise = 0 # How much noise is added to the data
-num_samples = 25   # Numbers of samples used in the belief propagation algorithm
+num_samples = 5   # Numbers of samples used in the belief propagation algorithm
 explore_perc = 0.1  # Fraction of uniform samples to keep exploring
 source_dim = 2      # The dimensionality of the incoming dataset (see "Load Dataset" below)
 target_dim = 1      # The number of dimensions the data is being reduced to
@@ -263,14 +263,14 @@ def weightPrior(ts_s, m_prev, neighbor_neighbor, neighbor, current):
 		weight_prior = weight_prior + (m_prev[u][t].weights[i] * weight)
 	return weight_prior
 
-def noisifyTS(ts):
-	theta = np.random.uniform(-10, 10) * np.pi / 180.0
+def noisifyTS(ts, var):
+	theta = np.random.normal(0, var) * np.pi / 180.0
 	rotMat = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 	return np.array([np.dot(rotMat, ts[0])])
 
-def noisifyTSList(ts_list):
+def noisifyTSList(ts_list, var=5):
 	for i in range(len(ts_list)):
-		ts_list[i] = noisifyTS(ts_list[i])
+		ts_list[i] = noisifyTS(ts_list[i], var)
 	return ts_list
 
 for iter_num in range(1, num_iters+1):
