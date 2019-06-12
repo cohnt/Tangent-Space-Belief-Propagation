1d4cd0ef2908fbd9b608ead976cfffe1d842835f
Thomas Cohn <cohnt@umich.edu>
2019-06-12 12:04:53 -0400

    Adjust image processing script to have longer delays and also include the dataset.

--- process_tangent_images.sh
+++ process_tangent_images.sh
@@ -9,8 +9,8 @@ mv *.svg svg
 
 cd ./png
 
-convert nearest_neighbors.png pca_observations.png ts_bel_iter*.png -set delay '%[fx:t==(n-1) || t==0 || t==1 ? 200 : 20]' belief.gif
-convert nearest_neighbors.png pca_observations.png ts_mle_iter*.png -set delay '%[fx:t==(n-1) || t==0 || t==1 ? 200 : 20]' mle.gif
+convert dataset.png nearest_neighbors.png pca_observations.png ts_bel_iter*.png -set delay '%[fx:t==(n-1) || t==0 || t==1 || t==2 ? 400 : 20]' belief.gif
+convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png -set delay '%[fx:t==(n-1) || t==0 || t==1 || t==2 ? 400 : 20]' mle.gif
 
 mv *.gif ../gif
 cd ..
