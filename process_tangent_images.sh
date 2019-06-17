3cdefd25ec5379e247b428188e60e27767a96096
Thomas Cohn <cohnt@umich.edu>
2019-06-17 19:37:57 -0400

    Needed slightly longer delay for one of them.

--- process_tangent_images.sh
+++ process_tangent_images.sh
@@ -18,7 +18,7 @@ convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png 
 convert error_histogram*.png -set delay '%[fx:t==(n-1) || t==0 ? 400 : 50]' error_histogram.gif
 
 convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png added_edges.png coord_bp.png \
--set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==(n-3) || t==0 || t==1 || t==2 ? 400 : 20]' result.gif
+-set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==(n-3) || t==(n-4) || t==0 || t==1 || t==2 ? 400 : 20]' result.gif
 
 mv *.gif ../gif
 cd ..
