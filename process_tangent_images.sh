74aedea09f554bb52946558f64670f0be537bc25
Thomas Cohn <cohnt@umich.edu>
2019-06-17 14:45:13 -0400

    Adjust timing of error histogram gif.

--- process_tangent_images.sh
+++ process_tangent_images.sh
@@ -11,7 +11,7 @@ cd ./png
 
 convert dataset.png nearest_neighbors.png pca_observations.png ts_bel_iter*.png pruned_nearest_neighbors.png -set delay '%[fx:t==(n-1) || t==(n-2) || t==0 || t==1 || t==2 ? 400 : 20]' belief.gif
 convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png -set delay '%[fx:t==(n-1) || t==(n-2) || t==0 || t==1 || t==2 ? 400 : 20]' mle.gif
-convert error_histogram*.png -set delay '%[fx:t==(n-1) ? 400 : 20]' error_histogram.gif
+convert error_histogram*.png -set delay '%[fx:t==(n-1) || t==0 ? 400 : 50]' error_histogram.gif
 
 mv *.gif ../gif
 cd ..
