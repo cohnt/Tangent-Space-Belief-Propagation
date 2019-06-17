317d8f3c87a0faf42c2cef2d25d1289a626eeed7
Thomas Cohn <cohnt@umich.edu>
2019-06-17 19:05:24 -0400

    Two new datasets to play with.

---
+++ datasets/dim_2/eight_curve.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.linalg import orth
+
+def make_eight_curve(n_samples, noise_factor):
+	"""
+		n_samples: number of points to generate
+		noise_factor: variance of the noise added to each dimension
+		
+		For best results, noise_factor should be pretty small (at most 0.001)
+	"""
+
+	# The s curve is parameterized by 
+	# x(t) = 0.5 + sin(t)cos(t)
+	# y(t) = 0.5 + 0.5cos(t)
+	# for (1/8)pi <= t <= (15/8)pi
+	# See for graph: https://www.desmos.com/calculator/ete1b4p535
+	lowerBound = 1.0 * np.pi / 8.0
+	upperBound = 15.0 * np.pi / 8.0
+	
+	# For computing the Jacobian, we have
+	# dx/dt = cos(t)cos(t) - sin(t)sin(t)
+	# dy/dt = -0.5sin(t)
+
+	t = np.random.uniform(lowerBound, upperBound, n_samples)
+	data = np.array([0.5 + np.multiply(np.sin(t), np.cos(t)), 0.5 + (0.5 * np.cos(t))]).transpose()
+	# data.shape will be (n_samples, 2), so we can interpret it as a list of n_samples 2D points
+	ts = np.array([(np.cos(t) ** 2) - (np.sin(t) ** 2), -0.5 * np.sin(t)]).transpose().reshape(n_samples, 1, 2)
+	for i in range(n_samples):
+		ts[i] = orth(ts[i].T).T
+
+	# Add Gaussian noise to the samples
+	mean = [0, 0]
+	cov = [[noise_factor, 0], [0, noise_factor]]
+	noise = np.random.multivariate_normal(mean, cov, n_samples)
+
+	color = (t - lowerBound) / (upperBound - lowerBound)
+
+	return (data + noise, color, ts)
+
+if __name__ == "__main__":
+	import matplotlib.pyplot as plt
+	data, color, ts = make_eight_curve(500, 0.001)
+	plt.scatter(data[:,0], data[:,1], c=color, cmap=plt.cm.Spectral)
+	plt.show()
