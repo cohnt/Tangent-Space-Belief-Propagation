f2f44123a063eda9aa95ba69293e82875a7ec2e1
Thomas Cohn <cohnt@umich.edu>
2019-06-17 19:05:09 -0400

    One more image to add.

--- process_tangent_images.sh
+++ process_tangent_images.sh
@@ -9,10 +9,16 @@ mv *.svg svg
 
 cd ./png
 
-convert dataset.png nearest_neighbors.png pca_observations.png ts_bel_iter*.png pruned_nearest_neighbors.png -set delay '%[fx:t==(n-1) || t==(n-2) || t==0 || t==1 || t==2 ? 400 : 20]' belief.gif
-convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png -set delay '%[fx:t==(n-1) || t==(n-2) || t==0 || t==1 || t==2 ? 400 : 20]' mle.gif
+convert dataset.png nearest_neighbors.png pca_observations.png ts_bel_iter*.png pruned_nearest_neighbors.png added_edges.png\
+-set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==0 || t==1 || t==2 ? 400 : 20]' belief.gif
+
+convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png added_edges.png\
+-set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==0 || t==1 || t==2 ? 400 : 20]' mle.gif
+
 convert error_histogram*.png -set delay '%[fx:t==(n-1) || t==0 ? 400 : 50]' error_histogram.gif
-convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png coord_bp.png -set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==0 || t==1 || t==2 ? 400 : 20]' result.gif
+
+convert dataset.png nearest_neighbors.png pca_observations.png ts_mle_iter*.png pruned_nearest_neighbors.png added_edges.png coord_bp.png\
+-set delay '%[fx:t==(n-1) || t==(n-2) || t==(n-3) || t==(n-3) || t==0 || t==1 || t==2 ? 400 : 20]' result.gif
 
 mv *.gif ../gif
 cd ..
