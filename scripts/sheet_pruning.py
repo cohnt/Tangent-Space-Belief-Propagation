7915ce695ada5fbfda095add2384e5eee12b2e04
Thomas Cohn <cohnt@umich.edu>
2019-08-01 18:18:59 -0400

    This looks better!

--- scripts/sheet_pruning.py
+++ scripts/sheet_pruning.py
@@ -17,7 +17,7 @@ global_t0 = time.time()
 dataset_name = "swiss_roll"
 dataset_seed = np.random.randint(0, 2**32)
 num_points = 1000    # Number of data points
-data_noise = 0.0001     # How much noise is added to the data
+data_noise = 0.0005     # How much noise is added to the data
 source_dim = 3      # The dimensionality of the incoming dataset (see "Load Dataset" below)
 target_dim = 2      # The number of dimensions the data is being reduced to
 
@@ -162,7 +162,7 @@ plot_neighbors_3d(points, color, neighbor_graph, ax, point_size=data_sp_rad, lin
 ax.set_title("Nearest Neighbors (k=%d)" % neighbors_k)
 plt.savefig(output_dir + "nearest_neighbors.svg")
 angles = np.linspace(0, 360, 40+1)[:-1]
-rotanimate(ax, angles, output_dir + "nearest_neighbors.gif", delay=30, width=14.4, height=10.8, folder=output_dir)
+rotanimate(ax, angles, output_dir + "nearest_neighbors.gif", delay=30, width=14.4, height=10.8, folder=output_dir, elevation=disp_elev)
 plt.close(fig)
 t1 = time.time()
 write("Done! dt=%f\n" % (t1-t0))
@@ -733,7 +733,7 @@ plot_neighbors_3d(points, color, pruned_neighbors, ax, point_size=data_sp_rad, l
 ax.set_title("Pruned Nearest Neighbors (k=%d, thresh=%f)" % (neighbors_k, pruning_angle_thresh))
 plt.savefig(output_dir + "pruned_nearest_neighbors.svg")
 angles = np.linspace(0, 360, 40+1)[:-1]
-rotanimate(ax, angles, output_dir + "pruned_nearest_neighbors.gif", delay=30, width=14.4, height=10.8, folder=output_dir)
+rotanimate(ax, angles, output_dir + "pruned_nearest_neighbors.gif", delay=30, width=14.4, height=10.8, folder=output_dir, elevation=disp_elev)
 plt.close(fig)
 
 write("Connecting graph...\n")
