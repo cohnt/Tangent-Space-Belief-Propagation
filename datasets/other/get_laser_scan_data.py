af2d5c0ef64dc80fa3a5dd794befd420a9d7d9bf
Thomas Cohn <cohnt@umich.edu>
2019-08-02 13:45:28 -0400

    Change the order of the try-except block to move the file write outside of it.

--- datasets/other/get_laser_scan_data.py
+++ datasets/other/get_laser_scan_data.py
@@ -24,8 +24,8 @@ t0 = time()
 rospy.init_node(node_name)
 
 iter_num = 1
-try:
-	while True:
+while True:
+	try:
 		print "Recording data #%d" % iter_num
 		data_message = rospy.wait_for_message(topic_name, LaserScanMsg)
 
@@ -45,26 +45,28 @@ try:
 		# Replace infinity with range_max
 		range_max = data_message.range_max
 		ranges[ranges == np.inf] = range_max
+	except KeyboardInterrupt:
+		break
 
-		######
+	######
 
-		# orig_data = np.multiply([np.cos(angles), -np.sin(angles)], data_message.ranges).transpose()
-		# inter_data = np.multiply([np.cos(angles), -np.sin(angles)], inter_ranges).transpose()
-		# final_data = np.multiply([np.cos(angles), -np.sin(angles)], ranges).transpose()
+	# orig_data = np.multiply([np.cos(angles), -np.sin(angles)], data_message.ranges).transpose()
+	# inter_data = np.multiply([np.cos(angles), -np.sin(angles)], inter_ranges).transpose()
+	# final_data = np.multiply([np.cos(angles), -np.sin(angles)], ranges).transpose()
 
-		# fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
-		# ax1.scatter(orig_data[:,0], orig_data[:,1])
-		# ax2.scatter(inter_data[:,0], inter_data[:,1])
-		# ax3.scatter(final_data[:,0], final_data[:,1])
-		# plt.show()
-		# exit()
+	# fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
+	# ax1.scatter(orig_data[:,0], orig_data[:,1])
+	# ax2.scatter(inter_data[:,0], inter_data[:,1])
+	# ax3.scatter(final_data[:,0], final_data[:,1])
+	# plt.show()
+	# exit()
 
-		######
+	######
 
-		str = ','.join([`num` for num in ranges])
-		data_file.write("%s\n" % str)
+	str = ','.join([`num` for num in ranges])
+	data_file.write("%s\n" % str)
 
-		sleep(rate)
-		iter_num = iter_num + 1
-except KeyboardInterrupt:
-	data_file.close()
+	sleep(rate)
+	iter_num = iter_num + 1
+
+data_file.close()
