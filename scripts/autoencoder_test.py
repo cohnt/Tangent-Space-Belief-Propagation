f723424b2c401c75d06b2515d8389d0cbd4d784b
Thomas Cohn <cohnt@umich.edu>
2020-01-22 14:12:12 -0500

    Build an autoencoder to embed 2D -> 1D.

---
+++ scripts/autoencoder_test.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from textwrap import wrap
+
+############
+# 2D -> 1D #
+############
+
+dataset_name = "long_spiral_curve"
+dataset_seed = 4045775215 # np.random.randint(0, 2**32)
+num_points = 500 # Number of data points
+data_noise = 0.0001 # How much noise is added to the data
+source_dim = 2 # The dimensionality of the incoming dataset (see "Load Dataset" below)
+target_dim = 1 # The number of dimensions the data is being reduced to
+
+data_sp_rad = 7.0
+data_sp_lw = 1.0
+embedding_sp_rad = 7.0
+embedding_sp_lw = 1.0
+
+from datasets.dim_2.arc_curve import make_arc_curve
+from datasets.dim_2.s_curve import make_s_curve
+from datasets.dim_2.o_curve import make_o_curve
+from datasets.dim_2.eight_curve import make_eight_curve
+from datasets.dim_2.long_spiral_curve import make_long_spiral_curve
+from sklearn.preprocessing import MinMaxScaler
+
+points, color, true_tangents, dataset_seed = make_long_spiral_curve(num_points, data_noise, rs_seed=dataset_seed)
+scaler = MinMaxScaler()
+data_scaled = scaler.fit_transform(points)
+
+fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
+ax.scatter(data_scaled[:,0], data_scaled[:,1], c=color, cmap=plt.cm.Spectral, s=data_sp_rad**2, zorder=2, linewidth=data_sp_lw)
+ax.set_title("Dataset (num=%d, variance=%f, seed=%d)\n" % (num_points, data_noise, dataset_seed))
+plt.show()
+
+############################
+
+from keras.layers import Input, Dense
+from keras.models import Model
+
+input_layer = Input(shape=(source_dim,))
+encoder_layer_1 = Dense(64, activation="tanh")(input_layer)
+encoder_layer_2 = Dense(32, activation="tanh")(encoder_layer_1)
+encoder_layer_3 = Dense(32, activation="tanh")(encoder_layer_2)
+choke_layer = Dense(target_dim, activation="tanh")(encoder_layer_3)
+decoder_layer_3 = Dense(32, activation="tanh")(choke_layer)
+decoder_layer_2 = Dense(32, activation="tanh")(decoder_layer_3)
+decoder_layer_1 = Dense(64, activation="tanh")(decoder_layer_2)
+output_layer = Dense(source_dim, activation="sigmoid")(decoder_layer_1)
+
+encoder = Model(input_layer, choke_layer)
+autoencoder = Model(input_layer, output_layer)
+
+train_test_cutoff = int(0.8 * num_points)
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+autoencoder.fit(
+	data_scaled[:train_test_cutoff], data_scaled[:train_test_cutoff],
+	epochs=50,
+	batch_size=256,
+	shuffle=True,
+	validation_data=(data_scaled[train_test_cutoff:], data_scaled[train_test_cutoff:])
+)
+
+embedded_points = encoder.predict(data_scaled)
+
+############################
+
+fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
+ax.scatter(color, embedded_points, c=color, cmap=plt.cm.Spectral, s=embedding_sp_rad**2, linewidths=embedding_sp_lw)
+ax.set_title("\n".join(wrap("Actual Parameter Value vs Embedded Coordinate from Autoencoder", 50)))
+plt.xlabel("Actual Parameter Value")
+plt.ylabel("Embedded Coordinate")
+plt.show()
