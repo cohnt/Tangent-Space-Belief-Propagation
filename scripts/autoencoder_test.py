34a033109fd00a5166a0d48e26d0b015feddaa6e
Thomas Cohn <cohnt@umich.edu>
2020-01-22 14:13:48 -0500

    Build an autoencoder to embed 3D -> 1D and another to embed 3D -> 2D.

--- scripts/autoencoder_test.py
+++ scripts/autoencoder_test.py
@@ -10,7 +10,7 @@ from textwrap import wrap
 dataset_name = "long_spiral_curve"
 dataset_seed = 4045775215 # np.random.randint(0, 2**32)
 num_points = 500 # Number of data points
-data_noise = 0.0001 # How much noise is added to the data
+data_noise = 0.001 # How much noise is added to the data
 source_dim = 2 # The dimensionality of the incoming dataset (see "Load Dataset" below)
 target_dim = 1 # The number of dimensions the data is being reduced to
 
@@ -73,4 +73,134 @@ ax.scatter(color, embedded_points, c=color, cmap=plt.cm.Spectral, s=embedding_sp
 ax.set_title("\n".join(wrap("Actual Parameter Value vs Embedded Coordinate from Autoencoder", 50)))
 plt.xlabel("Actual Parameter Value")
 plt.ylabel("Embedded Coordinate")
+plt.show()
+
+############
+# 3D -> 1D #
+############
+
+from mpl_toolkits.mplot3d import Axes3D
+
+dataset_name = "tight_spiral_curve"
+dataset_seed = np.random.randint(0, 2**32)
+num_points = 500 # Number of data points
+data_noise = 0.0005 # How much noise is added to the data
+source_dim = 3 # The dimensionality of the incoming dataset (see "Load Dataset" below)
+target_dim = 1 # The number of dimensions the data is being reduced to
+
+from datasets.dim_3.helix_curve import make_helix_curve
+from datasets.dim_3.tight_spiral_curve import make_tight_spiral_curve
+
+points, color, true_tangents, dataset_seed = make_tight_spiral_curve(num_points, data_noise, rs_seed=dataset_seed)
+scaler = MinMaxScaler()
+data_scaled = scaler.fit_transform(points)
+fig = plt.figure(figsize=(14.4, 10.8), dpi=100)
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(data_scaled[:,0], data_scaled[:,1], data_scaled[:,2], c=color, cmap=plt.cm.Spectral, s=data_sp_rad**2, linewidth=data_sp_lw)
+ax.set_title("Dataset (num=%d, variance=%f, seed=%d)" % (num_points, data_noise, dataset_seed))
+plt.show()
+
+############################
+
+input_layer = Input(shape=(source_dim,))
+encoder_layer_1 = Dense(64, activation="tanh")(input_layer)
+encoder_layer_2 = Dense(32, activation="tanh")(encoder_layer_1)
+encoder_layer_3 = Dense(32, activation="tanh")(encoder_layer_2)
+choke_layer = Dense(target_dim, activation="tanh")(encoder_layer_3)
+decoder_layer_3 = Dense(32, activation="tanh")(choke_layer)
+decoder_layer_2 = Dense(32, activation="tanh")(decoder_layer_3)
+decoder_layer_1 = Dense(64, activation="tanh")(decoder_layer_2)
+output_layer = Dense(source_dim, activation="sigmoid")(decoder_layer_1)
+
+encoder = Model(input_layer, choke_layer)
+autoencoder = Model(input_layer, output_layer)
+
+train_test_cutoff = int(0.8 * num_points)
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+autoencoder.fit(
+	data_scaled[:train_test_cutoff], data_scaled[:train_test_cutoff],
+	epochs=50,
+	batch_size=256,
+	shuffle=True,
+	validation_data=(data_scaled[train_test_cutoff:], data_scaled[train_test_cutoff:])
+)
+
+embedded_points = encoder.predict(data_scaled)
+
+############################
+
+fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
+ax.scatter(color, embedded_points, c=color, cmap=plt.cm.Spectral, s=embedding_sp_rad**2, linewidths=embedding_sp_lw)
+ax.set_title("\n".join(wrap("Actual Parameter Value vs Embedded Coordinate from Autoencoder", 50)))
+plt.xlabel("Actual Parameter Value")
+plt.ylabel("Embedded Coordinate")
+plt.show()
+
+############
+# 3D -> 2D #
+############
+
+dataset_name = "swiss_roll"
+dataset_seed = np.random.randint(0, 2**32)
+num_points = 350    # Number of data points
+data_noise = 0.001     # How much noise is added to the data
+source_dim = 3      # The dimensionality of the incoming dataset (see "Load Dataset" below)
+target_dim = 2      # The number of dimensions the data is being reduced to
+
+from datasets.dim_3.s_sheet import make_s_sheet
+from datasets.dim_3.swiss_roll import make_swiss_roll_sheet
+
+disp_elev = 5.0
+disp_azim = -85.0
+
+def make3DFigure():
+	f = plt.figure(figsize=(14.4, 10.8), dpi=100)
+	a = f.add_subplot(111, projection='3d')
+	if dataset_name == "swiss_roll":
+		a.set_ylim(bottom=-0.5, top=1.5)
+	a.view_init(elev=disp_elev, azim=disp_azim)
+	return f, a
+
+points, color, true_tangents, dataset_seed = make_swiss_roll_sheet(num_points, data_noise, rs_seed=dataset_seed)
+scaler = MinMaxScaler()
+data_scaled = scaler.fit_transform(points)
+fig, ax = make3DFigure()
+ax.scatter(data_scaled[:,0], data_scaled[:,1], data_scaled[:,2], c=color, cmap=plt.cm.Spectral, s=data_sp_rad**2, linewidth=data_sp_lw)
+ax.set_title("Dataset (num=%d, variance=%f, seed=%d)" % (num_points, data_noise, dataset_seed))
+plt.show()
+
+############################
+
+input_layer = Input(shape=(source_dim,))
+encoder_layer_1 = Dense(64, activation="tanh")(input_layer)
+encoder_layer_2 = Dense(32, activation="tanh")(encoder_layer_1)
+encoder_layer_3 = Dense(32, activation="tanh")(encoder_layer_2)
+choke_layer = Dense(target_dim, activation="tanh")(encoder_layer_3)
+decoder_layer_3 = Dense(32, activation="tanh")(choke_layer)
+decoder_layer_2 = Dense(32, activation="tanh")(decoder_layer_3)
+decoder_layer_1 = Dense(64, activation="tanh")(decoder_layer_2)
+output_layer = Dense(source_dim, activation="sigmoid")(decoder_layer_1)
+
+encoder = Model(input_layer, choke_layer)
+autoencoder = Model(input_layer, output_layer)
+
+train_test_cutoff = int(0.8 * num_points)
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+autoencoder.fit(
+	data_scaled[:train_test_cutoff], data_scaled[:train_test_cutoff],
+	epochs=50,
+	batch_size=256,
+	shuffle=True,
+	validation_data=(data_scaled[train_test_cutoff:], data_scaled[train_test_cutoff:])
+)
+
+embedded_points = encoder.predict(data_scaled)
+
+############################
+
+fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
+ax.scatter(embedded_points[:,0], embedded_points[:,1], c=color, cmap=plt.cm.Spectral, s=embedding_sp_rad**2, linewidths=embedding_sp_lw)
+ax.set_title("\n".join(wrap("2D Embedding from Autoencoder", 60)))
 plt.show()
