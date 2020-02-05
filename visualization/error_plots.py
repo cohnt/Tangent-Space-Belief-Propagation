0ccab1a12f3dd320e4bfd6355d13e47f4cf24f9d
Thomas Cohn <cohnt@umich.edu>
2020-02-05 17:22:16 -0500

    Code to produce a bar chart of the reconstruction error of each method.

---
+++ visualization/error_plots.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def relativeErrorBarChart(ax, dict):
+	rect = ax.bar(range(len(dict)), list(dict.values()), align='center')
+	ax.set_title("Reconstruction Error by Manifold Learning Algorithm")
+	ax.set_ylabel("Error")
+	ax.set_xticks(range(len(dict)))
+	ax.set_xticklabels(list(dict.keys()))
+	ax.tick_params(axis='x', which='major', labelsize=10)
+
+	autolabel(rect)
+
+def autolabel(rects, xpos='center'):
+	xpos = xpos.lower()
+	ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+	offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
+
+	for rect in rects:
+		height = rect.get_height()
+		ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,
+		        '{}'.format(height), ha=ha[xpos], va='bottom')
