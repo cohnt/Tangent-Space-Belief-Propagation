4a1dd7eebd1f1c7c532ea27c7e014be095b9556e
Thomas Cohn <cohnt@umich.edu>
2020-02-07 17:16:14 -0500

    Don't normalize in-place.

--- utils.py
+++ utils.py
@@ -86,7 +86,7 @@ def increaseDimensionMatrix(old_dimension, new_dimension):
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import normalize
 def pairwiseDistErr(embedded_points, true_parameters, dist_metric="l2", mat_norm="fro"):
-	embedded_dists = pairwise_distances(normalize(embedded_points, axis=1, copy=False), metric=dist_metric, n_jobs=-1)
-	true_dists = pairwise_distances(normalize(true_parameters, axis=1, copy=False), metric=dist_metric, n_jobs=-1)
+	embedded_dists = pairwise_distances(normalize(embedded_points, axis=1, copy=True), metric=dist_metric, n_jobs=-1)
+	true_dists = pairwise_distances(normalize(true_parameters, axis=1, copy=True), metric=dist_metric, n_jobs=-1)
 	err = np.linalg.norm(embedded_dists - true_dists, ord=mat_norm, axis=None)
 	return err
