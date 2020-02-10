I was normalizing along the wrong axis!

utils.py
@@ -89,12 +89,11 @@ def pairwiseDistErr(embedded_points, true_parameters, normalize_data=True, norma
 	embedded_dists = None
 	true_dists = None
 	if normalize_data:
-		embedded_dists = pairwise_distances(normalize(embedded_points, axis=1, copy=True), metric=dist_metric, n_jobs=-1)
-		true_dists = pairwise_distances(normalize(true_parameters, axis=1, copy=True), metric=dist_metric, n_jobs=-1)
+		embedded_dists = pairwise_distances(normalize(embedded_points, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
+		true_dists = pairwise_distances(normalize(true_parameters, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
 	else:
 		embedded_dists = pairwise_distances(embedded_points, metric=dist_metric, n_jobs=-1)
 		true_dists = pairwise_distances(true_parameters, metric=dist_metric, n_jobs=-1)
-
 	err = None
 	if normalize_dists:
 		err = np.linalg.norm((embedded_dists/np.max(embedded_dists)) - (true_dists/np.max(true_dists)), ord=mat_norm, axis=None)