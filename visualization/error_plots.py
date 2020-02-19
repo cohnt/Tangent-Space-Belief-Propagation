cd77d1c9a20c2d1bcc4367d13218d7529b180447
Thomas Cohn <cohnt@umich.edu>
2020-02-18 21:56:53 -0500

    Add a density setting, to make the plots a bit easier to draw.

--- visualization/error_plots.py
+++ visualization/error_plots.py
@@ -23,15 +23,16 @@ def autolabel(ax, rects, xpos='center'):
 
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import normalize
-def regressionErrorCharacteristic(ax, embedded_points, true_parameters, dist_metric="l2"):
+def regressionErrorCharacteristic(ax, embedded_points, true_parameters, dist_metric="l2", density=0.001):
 	embedded_dists = pairwise_distances(normalize(embedded_points, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
 	true_dists = pairwise_distances(normalize(true_parameters, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
 	err_mat = np.abs(embedded_dists - true_dists)
 
 	num_points = err_mat.shape[0]
 	x_vals = np.sort(err_mat[np.triu_indices(num_points)])
+	x_vals = x_vals[np.asarray(np.linspace(0, len(x_vals)-1, num=density*len(x_vals)), dtype=int)]
 	y_vals = np.arange(1, len(x_vals)+1) / float(len(x_vals))
-	ax.scatter(x_vals, y_vals)
+	ax.plot(x_vals, y_vals)
 	ax.set_xlim(left=0, right=np.max(err_mat))
 	ax.set_ylim(bottom=0, top=1)
 	ax.set_xlabel("Distance Error Threshold")
