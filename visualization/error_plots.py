90151e8f7cd14a742a2de89982f8733ae07d8a37
Thomas Cohn <cohnt@umich.edu>
2020-02-18 22:27:19 -0500

    Function to draw several RECs on one plot.

--- visualization/error_plots.py
+++ visualization/error_plots.py
@@ -37,3 +37,28 @@ def regressionErrorCharacteristic(ax, embedded_points, true_parameters, dist_met
 	ax.set_ylim(bottom=0, top=1)
 	ax.set_xlabel("Distance Error Threshold")
 	ax.set_ylabel("Proportion of Pairwise Distances")
+
+import matplotlib.cm as cm
+def listRegressionErrorCharacteristic(ax, embedded_points_list, true_parameters, name_list, dist_metric="l2", density=0.001):
+	max_err = 0
+	colors = cm.rainbow(np.linspace(0, 1, len(embedded_points_list)))
+	for i in range(len(embedded_points_list)):
+		embedded_points = embedded_points_list[i]
+		embedded_dists = pairwise_distances(normalize(embedded_points, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
+		true_dists = pairwise_distances(normalize(true_parameters, axis=0, copy=True), metric=dist_metric, n_jobs=-1)
+		err_mat = np.abs(embedded_dists - true_dists)
+		if np.max(err_mat) > max_err:
+			max_err = np.max(err_mat)
+
+		num_points = err_mat.shape[0]
+		x_vals = np.sort(err_mat[np.triu_indices(num_points)])
+		x_vals = x_vals[np.asarray(np.linspace(0, len(x_vals)-1, num=density*len(x_vals)), dtype=int)]
+		y_vals = np.arange(1, len(x_vals)+1) / float(len(x_vals))
+		ax.plot(x_vals, y_vals, label=name_list[i], color=colors[i])
+
+	ax.set_xlim(left=0, right=max_err)
+	ax.set_ylim(bottom=0, top=1)
+	ax.set_xlabel("Distance Error Threshold")
+	ax.set_ylabel("Proportion of Pairwise Distances")
+
+	ax.legend()
