e6fd126f1c0475d13823fcec1b8dc034dc7e10c9
Thomas Cohn <cohnt@umich.edu>
2020-02-22 14:17:47 -0500

    Recreating a specific test.

--- scripts/sheet_pruning.py
+++ scripts/sheet_pruning.py
@@ -16,8 +16,9 @@ from collections import OrderedDict
 global_t0 = time.time()
 
 dataset_name = "swiss_roll"
-dataset_seed = np.random.randint(0, 2**32)
-num_points = 350    # Number of data points
+# dataset_seed = np.random.randint(0, 2**32)
+dataset_seed = 2007610755
+num_points = 500    # Number of data points
 data_noise = 0.001     # How much noise is added to the data
 source_dim = 3      # The dimensionality of the incoming dataset (see "Load Dataset" below)
 target_dim = 2      # The number of dimensions the data is being reduced to
@@ -192,7 +193,7 @@ t0 = time.time()
 fig, ax = make3DFigure()
 # Not squared because it's squared inside plot_neighbors_3d
 plot_neighbors_3d(points, color, neighbor_graph, ax, point_size=data_sp_rad, line_width=data_sp_lw, edge_thickness=nn_lw, show_labels=False, line_color="grey")
-ax.set_title("Nearest Neighbors (k=%d)" % neighbors_k)
+ax.set_title("Nearest Neighbors (k=%d)\n" % neighbors_k)
 setAxisTickSize3D(ax, neighbors_axis_tick_size)
 plt.savefig(output_dir + "nearest_neighbors.svg")
 angles = np.linspace(0, 360, 40+1)[:-1]
@@ -742,7 +743,7 @@ flush()
 
 fig, ax = make3DFigure()
 plot_neighbors_3d(points, color, pruned_neighbors, ax, point_size=data_sp_rad, line_width=data_sp_lw, edge_thickness=nn_lw, show_labels=False, line_color="grey")
-ax.set_title("Pruned Nearest Neighbors (k=%d, thresh=%f)" % (neighbors_k, pruning_angle_thresh))
+ax.set_title("Pruned Nearest Neighbors (k=%d, thresh=%f)\n" % (neighbors_k, pruning_angle_thresh))
 setAxisTickSize3D(ax, neighbors_axis_tick_size)
 plt.savefig(output_dir + "pruned_nearest_neighbors.svg")
 angles = np.linspace(0, 360, 40+1)[:-1]
@@ -808,7 +809,7 @@ flush()
 
 fig, ax = make3DFigure()
 plot_neighbors_3d(points, color, pruned_neighbors, ax, point_size=data_sp_rad, line_width=data_sp_lw, edge_thickness=nn_lw, show_labels=False, line_color="grey")
-ax.set_title("Added Edges after Pruning")
+ax.set_title("Added Edges after Pruning\n")
 setAxisTickSize3D(ax, neighbors_axis_tick_size)
 plt.savefig(output_dir + "added_edges.svg")
 angles = np.linspace(0, 360, 40+1)[:-1]
