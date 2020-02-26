a93670819c50774c440b842a0353fd4c20161965
Thomas Cohn <cohnt@umich.edu>
2020-02-26 17:17:54 -0500

    Add the wrapper function that actually saves the images.

--- visualization/unroll.py
+++ visualization/unroll.py
@@ -1,17 +1,29 @@
 import numpy as np
-import matplotlib.plt as pyplot
+import matplotlib.pyplot as plt
 
 from plot_neighbors import plot_neighbors_2d
 
-def unroll(points, color, true_parameters, neighborhood_graph, num_images=50, line_color="grey", point_size=5, line_width=1, edge_thickness=1, show_labels=False):
+def unrollAnimation(points, color, true_parameters, neighbor_graph, base_filename, num_images=50, delay=10, line_color="grey", point_size=5, line_width=1, edge_thickness=1, show_labels=False):
+	points_by_frame = unrollGetPoints(points, true_parameters, num_images)
+	print "Got points!"
+	for i in range(len(points_by_frame)):
+		point_set = points_by_frame[i]
+		fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
+		plot_neighbors_2d(point_set, color, neighbor_graph, ax, point_size=point_size, line_width=line_width, edge_thickness=edge_thickness, show_labels=False)
+		plt.savefig(base_filename + "%03d" % i + ".png")
+		plt.close(fig)
+
+def unrollGetPoints(points, true_parameters, num_images):
 	x_vals, y_vals = np.transpose(points)
 	goal_x_vals = true_parameters[:,0]
 	goal_y_vals = np.zeros(len(true_parameters))
 	points_by_frame = []
 	num_points = len(points)
 	for i in range(0, num_images):
+		print "Image %d" % i
 		points_by_frame.append(points.copy())
 		for j in range(len(points)):
+			print "\tPoint %d" % j
 			x_min = x_vals[j]
 			x_max = goal_x_vals[j]
 			y_min = y_vals[j]
@@ -20,4 +32,4 @@ def unroll(points, color, true_parameters, neighborhood_graph, num_images=50, li
 			x = x_min + (t * (x_max - x_min))
 			y = y_min + (t * (y_max - y_min))
 			points_by_frame[i][j,:] = [x, y]
-	# print points_by_frame
+	return points_by_frame
