7b8b4b3512e18bccf61ed47bee3d724a262e57f8
Thomas Cohn <cohnt@umich.edu>
2020-02-26 17:35:22 -0500

    Better normalization.

--- visualization/unroll.py
+++ visualization/unroll.py
@@ -4,18 +4,24 @@ import matplotlib.pyplot as plt
 from plot_neighbors import plot_neighbors_2d
 
 def unrollAnimation(points, color, true_parameters, neighbor_graph, base_filename, num_images=50, delay=10, line_color="grey", point_size=5, line_width=1, edge_thickness=1, show_labels=False):
-	points_by_frame = unrollGetPoints(points, true_parameters, num_images)
+	min_x = np.min(points[:,0])
+	max_x = np.max(points[:,0])
+	min_y = np.min(points[:,1])
+	max_y = np.max(points[:,1])
+	points_by_frame = unrollGetPoints(points, true_parameters, num_images, embedding_y_val = (min_y + max_y) / 2.0)
 	for i in range(len(points_by_frame)):
 		point_set = points_by_frame[i]
 		fig, ax = plt.subplots(figsize=(14.4, 10.8), dpi=100)
 		plot_neighbors_2d(point_set, color, neighbor_graph, ax, point_size=point_size, line_width=line_width, edge_thickness=edge_thickness, show_labels=False)
+		ax.set_xlim((min_x, max_x))
+		ax.set_ylim((min_y, max_y))
 		plt.savefig(base_filename + "%03d" % i + ".png")
 		plt.close(fig)
 
-def unrollGetPoints(points, true_parameters, num_images):
+def unrollGetPoints(points, true_parameters, num_images, embedding_y_val=0):
 	x_vals, y_vals = np.transpose(points)
-	goal_x_vals = true_parameters[:,0]
-	goal_y_vals = np.zeros(len(true_parameters))
+	goal_x_vals = (true_parameters[:,0] - np.min(true_parameters[:,0])) / (np.max(true_parameters[:,0]) - np.min(true_parameters[:,0]))
+	goal_y_vals = np.zeros(len(true_parameters)) + embedding_y_val
 	points_by_frame = []
 	num_points = len(points)
 	for i in range(0, num_images):
