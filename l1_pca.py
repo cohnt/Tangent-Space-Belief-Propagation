14d046303aaeab08ce3b65081dd54e71b37ad454
Thomas Cohn <cohnt@umich.edu>
2020-05-28 12:38:33 -0400

    Compute the best B candidate.

--- l1_pca.py
+++ l1_pca.py
@@ -1,6 +1,17 @@
 import numpy as np
 
-# Using this algorithm.
+# Using this algorithm https://iehttps://ieeexplore-ieee-org.proxy.lib.umich.edu/stamp/stamp.jsp?tp=&arnumber=6629811eexplore-ieee-org.proxy.lib.umich.edu/stamp/stamp.jsp?tp=&arnumber=6629811
+
+def l1_pca(X, K):
+	D, N = X.shape
+	B_candidates = generate_matrices(N, K)
+	best_ind = 0
+	best_norm_val = np.linalg.norm(np.matmul(X, B_candidates[0]), ord="nuc")
+	for i in range(1, len(B_candidates)):
+		norm_val = np.linalg.norm(np.matmul(X, B_candidates[i]), ord="nuc")
+		if norm_val > best_norm_val:
+			best_norm_val = norm_val
+			best_ind = i
 
 def generate_matrices(N, K):
 	num_mats = 2**(N*K)
